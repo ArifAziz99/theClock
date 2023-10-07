6c97db7614c8d7073f3df67df68cd2858c533885
Mohammad Arif Aziz <arifaziz2743@gmail.com>
2023-10-07 16:47:22 +0530

    some tweaks

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function currentTime() {
+var clockEl = document.querySelector('#clock');
+
+function getTime() {
+  return new Date().toLocaleTimeString('en-US', 
+     { hour12: false, hour: 'numeric', minute: 'numeric' }).toString();
+}
+
+function setTime() {
+  var time = getTime();
+  // check if the colon is there
+  if (clockEl.innerText.split(':').length === 1) {
+    // if it's not, set the actual time
+    clockEl.innerText = time;
+  } else {
+    // if it is, remove the colon
+    clockEl.innerText = time.split(':').join(' ');
+  }
+}
+
+setInterval( setTime , 1000);
+setTime();
+
+
+
+
+/*function currentTime() {
   let date = new Date(); 
   let hh = date.getHours();
   let mm = date.getMinutes();
@@ -21,4 +46,4 @@ function currentTime() {
 
 }
 
-currentTime();
+currentTime();*/
